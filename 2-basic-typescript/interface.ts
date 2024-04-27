9cd51354763eb5a2e4220d32f64882dce275ad0e
sokusekimen <m.yamagishi9000@gmail.com>
2024-04-27 14:34:55 +0900

    2.3.4追記

--- 2-basic-typescript/interface.ts
+++ 2-basic-typescript/interface.ts
@@ -1,7 +1,7 @@
 // 2.3.4 インターフェース p49
 
-// セミコロンで終わっている注意
 interface Point {
+  // 型エイリアスとは違い、セミコロンで終わっている注意
   x: number;
   y: number;
 }
@@ -12,10 +12,19 @@ function prigntPoint(point: Point) {
   console.log(point.z);
 }
 
-// 上書きできる
+// 上書き拡張できる
 interface Point {
   z: number;
 }
 
 // これだとコンパイルエラーになる
 prigntPoint({x: 100, y: 200});
+
+// 継承もできる
+interface Colorful {
+  color: string;
+}
+interface Circle {
+  radius: number;
+}
+interface ColorfulCircle extends Colorful, Circle {}
