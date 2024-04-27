c11c63bb9c64a5f7b255ffeff3d065fe54818515
sokusekimen <m.yamagishi9000@gmail.com>
2024-04-27 16:14:53 +0900

    2.5.7

---
+++ 2-basic-typescript/unknown.ts
@@ -0,0 +1,13 @@
+// 2.5.7 unknown p63
+// anyと同様どんな型でも代入できるが、型安全な状況でしか使用できない
+
+// どんな値でも代入できる
+const x: unknown = 123;
+
+// ただしそのままでは使用できない
+console.log(x.toFixed(1)); // NG
+
+// 型安全な状況下でアクセス可能になる
+if (typeof x === 'number') {
+  console.log(x.toFixed(1)); // OK
+}
