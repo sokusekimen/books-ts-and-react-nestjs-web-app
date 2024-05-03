285c7f29b057b927ed0ceb5de290b5b097d3e6df
sokusekimen <m.yamagishi9000@gmail.com>
2024-05-03 20:12:56 +0900

    jestの追加

--- next-gihyo-book/next.config.mjs
+++ next-gihyo-book/next.config.mjs
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
+  compiler: (() => {
+    let compilerConfig = {
+      styledComponents: true,
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        // 本番環境ではReact Tesiting Libraryで使用するdata-test-id属性を削除
+        reactRemoveProperties: { properties: ['^data-testid$'] },
+      }
+    }
+
+    return compilerConfig
+  })(),
 };
 
 export default nextConfig;
