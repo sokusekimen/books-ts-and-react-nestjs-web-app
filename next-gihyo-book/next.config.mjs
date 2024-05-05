addf5556ce4bce3374f7575cdae5e90dfa881baa
sokusekimen <m.yamagishi9000@gmail.com>
2024-05-05 16:26:21 +0900

    componentsまで追加

--- next-gihyo-book/next.config.mjs
+++ next-gihyo-book/next.config.mjs
@@ -16,6 +16,15 @@ const nextConfig = {
 
     return compilerConfig
   })(),
+  // CORS対策でAPIプロキシを設定する
+  async rewrites() {
+    return [
+      {
+        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:match`,
+        destination: `${process.env.API_BASE_UURL}/:match`
+      }
+    ]
+  },
 };
 
 export default nextConfig;
